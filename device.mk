57f748cc8d437afcd3680b3fcc71b07c23381f4b
multiwirth <multiwirth@googlemail.com>
2021-07-27 08:44:07 +0200

    add FM2 package to device.mk to build fmradio app
    Change-Id: I2af9c35daa8711826b05ba64c8a1e33222a25dda

--- device.mk
+++ device.mk
@@ -104,6 +104,12 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     tinymix
 
+# FM Radio
+PRODUCT_PACKAGES += \
+    FM2 \
+    libqcomfm_jni \
+    qcom.fmradio
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/configs/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
